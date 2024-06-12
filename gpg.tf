e1aa881a540b01be952ce109a729f6360f53cfee
Oleksandr Kuzminskyi <aleks@infrahouse.com>
2024-06-12 13:40:40 -0700

    Apply permissions policy on the GPG secrets
    To grant access to the secrets, the module accepts variables:
    
    * signing_key_readers
    * signing_key_writers

--- gpg.tf
+++ gpg.tf
@@ -2,21 +2,23 @@ resource "random_password" "passphrase" {
   length = 21
 }
 
-resource "aws_secretsmanager_secret" "passphrase" {
-  name                    = "packager-passphrase-${var.repository_codename}"
-  description             = "Passphrase for a signing GPG key for ${var.repository_codename}"
-  recovery_window_in_days = 0
-  tags                    = local.tags
+module "passphrase" {
+  source             = "registry.infrahouse.com/infrahouse/secret/aws"
+  version            = "0.5.0"
+  secret_description = "Passphrase for a signing GPG key for ${var.repository_codename}"
+  secret_name        = "packager-passphrase-${var.repository_codename}"
+  secret_value       = random_password.passphrase.result
+  tags               = local.tags
+  readers            = var.signing_key_readers
+  writers            = var.signing_key_writers
 }
 
-resource "aws_secretsmanager_secret_version" "passphrase" {
-  secret_id     = aws_secretsmanager_secret.passphrase.id
-  secret_string = random_password.passphrase.result
-}
-
-resource "aws_secretsmanager_secret" "key" {
-  name                    = "packager-key-${var.repository_codename}"
-  description             = "Signing GPG key for ${var.repository_codename}"
-  recovery_window_in_days = 0
-  tags                    = local.tags
+module "key" {
+  source             = "registry.infrahouse.com/infrahouse/secret/aws"
+  version            = "0.5.0"
+  secret_description = "Signing GPG key for ${var.repository_codename}"
+  secret_name        = "packager-key-${var.repository_codename}"
+  tags               = local.tags
+  readers            = var.signing_key_readers
+  writers            = var.signing_key_writers
 }
