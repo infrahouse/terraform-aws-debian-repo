907b5c829a68e442e9e92a56916e9258aabc571e
Maksym Kryva <maksym@tinyfish.io>
2025-09-17 11:12:56 +0300

    Update dns.tf
    Signed-off-by: Maksym Kryva <maksym@tinyfish.io>

--- dns.tf
+++ dns.tf
@@ -16,8 +16,6 @@ resource "aws_route53_record" "caa_repo" {
   ttl     = 300
   records = [
     "0 issue \"amazon.com\"",
-    "0 issue \"amazontrust.com\"",
-    "0 issue \"awstrust.com\"",
-    "0 issue \"amazonaws.com\""
+    "0 issuewild \";\""
   ]
 }
